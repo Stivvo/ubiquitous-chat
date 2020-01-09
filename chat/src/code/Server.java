c2c8f72fd9dde24670f33e4bc7ba8dfc1c20b1d1
Stivvo <entattis15@itisvinci.com>
2020-01-09 22:10:55 +0100

    serverThread only for the server

--- chat/src/code/Server.java
+++ chat/src/code/Server.java
@@ -2,30 +2,51 @@ package code;
 
 import javax.xml.crypto.Data;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
-public class Server {
-    public static void main(String[] args) throws IOException {
-        DatagramSocket receiveSock = new DatagramSocket(6787);
-        MulticastSocket sendSock = new MulticastSocket();
-        sendSock.joinGroup(InetAddress.getByName("225.4.5.6"));
+class ServerThread extends Thread {
+    public static Semaphore sendMaphore = new Semaphore(0);
+    public static MulticastSocket sendSock;
 
-        ArrayList<Pair<DatagramPacket, String>> clients = new ArrayList<Pair<DatagramPacket, String>>();
+    static {
+        try {
+            sendSock = new MulticastSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Pair<Socket, String>> clients = new ArrayList<Pair<Socket, String>>();
+    public Socket receiveSock;
+
+    public ServerThread(Socket socket) {
+        try {
+            this.receiveSock = socket;
+            start();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doStuff() throws Exception {
+        BufferedReader fromClient = new BufferedReader(new InputStreamReader(receiveSock.getInputStream()));
+        DataOutputStream toClient = new DataOutputStream(receiveSock.getOutputStream());
         BufferedReader kybrd = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-            byte[] inBytes = new byte[1024];
-            DatagramPacket receivePack = new DatagramPacket(inBytes, inBytes.length);
-            receiveSock.receive(receivePack);
-            String strReceive = new String(receivePack.getData());
-            strReceive = strReceive.substring(0, receivePack.getLength());
+            String strReceive = fromClient.readLine();
+            if (strReceive.equals("end")) // client disconnected
+                break;
 
+            sendMaphore.acquire();
             boolean foundPort = false, foundName = false;
-            for (Pair<DatagramPacket, String> i : clients) { // search user and port in clients
-                if (i.getKey().getPort() == receivePack.getPort())
+            for (Pair<Socket, String> i : clients) { // search user and port in clients
+                if (i.getKey().getPort() == receiveSock.getPort())
                     foundPort = true;
                 if (i.getValue().equals(strReceive))
                     foundName = true;
@@ -38,11 +59,13 @@ public class Server {
                     strSend = "ko";
                 else { // free username
                     strSend = "yes";
-                    clients.add(new Pair<>(receivePack, strReceive));
+                    clients.add(new Pair<>(receiveSock, strReceive));
                 }
-                System.out.println("new client: port = " + receivePack.getPort() + ", name: " + strReceive +
+                System.out.println("new client: port = " + receiveSock.getPort() + ", name: " + strReceive +
                         "already used username = " + foundName + ", allow: " + strSend);
+                sendMaphore.release();
             } else { // an already connected cliend sent a message
+                sendMaphore.release();
                 System.out.println(strReceive + "\nallow? [Y/n] ");
                 if (!kybrd.readLine().equals("n")) { // allow the message?
                     byte[] outBytes = strReceive.getBytes();
@@ -53,12 +76,30 @@ public class Server {
                     System.out.println("not allowed");
                 strSend = "yes";
             }
-            // confirm to Client 
-            byte[] outBytes = strSend.getBytes();
-            DatagramPacket sendPack = new DatagramPacket(
-                    outBytes, outBytes.length, receivePack.getAddress(), receivePack.getPort());
-            receiveSock.send(sendPack);
+            toClient.writeBytes(strSend + "\n"); // confirm to Client
+            // remind server address
             System.out.println("server address: " + InetAddress.getLocalHost().getHostAddress());
+        }
+    }
+
+    public void run() {
+        try {
+            doStuff();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Server {
+    public static void main(String[] args) throws Exception {
+        ServerThread.sendSock.joinGroup(InetAddress.getByName("225.4.5.6"));
+        ServerThread.sendMaphore.release();
+        ServerSocket receiveServerSock = new ServerSocket(6789);
+
+        while (true) {
+            Socket socket = receiveServerSock.accept();
+            ServerThread serverThread = new ServerThread(socket);
         }
     }
 }
